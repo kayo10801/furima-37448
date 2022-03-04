44177e7cc8c3a7b8ef8de491de797fb3e093e7d4
kayo10801 <98790416+kayo10801@users.noreply.github.com>
2022-03-04 12:03:23 +0900

    テストコード

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -8,6 +8,7 @@ class FurimasController < ApplicationController
   end  
 
   def create
+  
+    
   end
-
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {'abcd1234'}
+    password_confirmation {password}
+    last_name             {'全角'}
+    first_name            {'全角'}
+    last_name_kana        {'カナ'}
+    first_name_kana       {'カナ'}
+    birth_day             {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    
+  end
+end
