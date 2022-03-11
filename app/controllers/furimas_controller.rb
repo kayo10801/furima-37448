db3ec29972df84d0a7e8e2b6ee70082d1d1a0156
kayo10801 <98790416+kayo10801@users.noreply.github.com>
2022-03-11 12:52:07 +0900

    商品編集機能修正

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,6 +1,6 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_furima, except: [:index, :new, :create]
 
   def index
     @furimas = Furima.includes(:user).order("created_at DESC")
@@ -22,20 +22,17 @@ class FurimasController < ApplicationController
   end
 
   def destroy
-    @furima = Furima.find(params[:id])
   end
   
 
 
   def edit
-    @furima = Furima.find(params[:id])
     unless @furima.user_id == current_user.id
     redirect_to root_path
     end
   end
   
   def update
-    @furima = Furima.find(params[:id])
     if @furima.update(furima_params)
        redirect_to furima_path(@furima)
     else
@@ -44,9 +41,8 @@ class FurimasController < ApplicationController
   end
 
     
-   def show
-     @furima = Furima.find(params[:id])
-   end
+  def show
+  end
 
   private
 
@@ -54,5 +50,8 @@ class FurimasController < ApplicationController
     params.require(:furima).permit(:image, :name, :introduction, :category_id, :condition_id, :postage_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_furima
+    @furima = Furima.find(params[:id])
+  end 
 
 end
