718b476b55214269e4ff5f5be2b7d3d3f0548a87
kayo10801 <98790416+kayo10801@users.noreply.github.com>
2022-03-16 03:01:30 +0900

    Update purchase_records_controller.rb

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,5 @@
 class PurchaseRecordsController < ApplicationController
-  before_action :authenticate_user!, [:show]
+  before_action :authenticate_user!, except: [:show]
   before_action :set_furima, only: [:index, :create]
   before_action :moov, only: [:index, :create]
 
